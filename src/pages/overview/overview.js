c7fa502bef916b91e0486ac8fa555de08fa3bd81
joanne <prince811009fy@gmail.com>
2020-03-24 21:44:10 +0800

    Update: add barchart in overview.js

--- src/pages/overview/overview.js
+++ src/pages/overview/overview.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import LayoutWrapper from '../../components/layoutWrapper'
 import DashboardCard from '../../components/card'
+import Main from '../../components/barchart/barchart'
 import OverviewTable from './overview-table'
 
 const Overview = () => (
   <div>
     <LayoutWrapper>
       <DashboardCard />
+      <Main />
       <OverviewTable />
     </LayoutWrapper>
   </div>
--- src/pages/userAdmin/index.js
+++ src/pages/userAdmin/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import LayoutWrapper from '../../components/layoutWrapper'
-import OverviewContent from '../overview/overview-content'
+import OverviewContent from '../overview/overview'
 
 import './index.scss'
 
